d16f9f61244e2e285bd4fc1479f82313ebeb7521
Aleksandr Pavlov <vertex55reg@gmail.com>
2020-07-24 22:33:23 +0300

    init

---
+++ app/src/main/java/com/github/vertex13/gameslist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.vertex13.gameslist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
